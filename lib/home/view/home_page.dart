6e5ebaadc0750bd03b14f2234852516ccb707782
Miguel Yurivilca <miguel.yurivilca.16@gmail.com>
2022-08-01 14:33:22 -0500

    chore: initial commit

---
+++ lib/home/view/home_page.dart
@@ -0,0 +1,15 @@
+import 'package:auth_example/home/home.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => HomeBloc(),
+      child: const HomeView(),
+    );
+  }
+}
---
+++ lib/login/view/login_page.dart
@@ -0,0 +1,15 @@
+import 'package:auth_example/login/login.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => LoginBloc(),
+      child: const LoginView(),
+    );
+  }
+}
